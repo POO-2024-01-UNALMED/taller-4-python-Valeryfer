2da3ba377cff62906b105fe5815f95f3640a7d91
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-03-05 09:22:52 -0500

    Primer intento

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -8,11 +8,13 @@ class Grupo:
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self,** kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista == None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
